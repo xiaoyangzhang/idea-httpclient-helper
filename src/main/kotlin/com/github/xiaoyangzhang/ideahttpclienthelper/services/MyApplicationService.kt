a2443457d48d7a7d40d93727cb74c4359271b94c
GitHub Action <action@github.com>
2021-11-09 12:12:43 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xiaoyangzhang/ideahttpclienthelper/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xiaoyangzhang.ideahttpclienthelper.services
+
+import com.github.xiaoyangzhang.ideahttpclienthelper.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
